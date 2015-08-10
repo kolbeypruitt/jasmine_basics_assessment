bd4aef2d638d0aa4011fd9b10e8add3c432e11f3
kolbeypruitt <kolbeypruitt@gmail.com>
2015-08-10 09:03:55 -0600

    all tests created!

--- spec/basics_spec.js
+++ spec/basics_spec.js
@@ -1,36 +1,21 @@
-var code = require('../something.js')
-describe("Hello World", function() {
-  it("says 'hello, world!' when ran", function() {
-    expect(code.outputHelloWorld()).toEqual("Hello, world!");
+var code = require('../basics.js');
+
+  describe('letterGrader', function(){
+    it('score should result in the a letter grade', function(){ expect(code.letterGrader(93)).toEqual('A-'); expect(code.letterGrader(87)).toEqual('B+'); expect(code.letterGrader(72)).toEqual('C-');
   });
 });
 
-describe('Tax Calculator', function(){
-
-  it('should tax 10% on the first $10', function(){ expect(code.calculate(1)).toEqual(0.1); expect(code.calculate(10)).toEqual(1); });
-
-  it('should tax 7% on the second $10', function(){ expect(code.calculate(15)).toEqual(1.35); });
-
-  it('should tax 5% on the third $10', function(){ expect(code.calculate(25)).toEqual(1.95); });
-
-  it('should tax 3% on the third $10', function(){ expect(code.calculate(35)).toEqual(2.35); });
+  describe('averageScore', function(){
+    it('should result in average of scores in the array', function(){ expect(code.averageScore([90, 95, 87, 60, 59, 100, 96, 74, 69])).toEqual(81);
+  });
 });
 
-describe('Leap Year', function(){
-  it('Years divisible by 4 should be', function(){ expect(code.leapYear(4)).toEqual(true); });
-
-  it('except years divisible by 100 and not 400', function(){ expect(code.leapYear(100)).toEqual(false); });
-
-  it('except years divisible by 100 and not 400', function(){ expect(code.leapYear(1700)).toEqual(false); });
+  describe('medianScore', function(){
+    it('should result in median number of array', function(){ expect(code.medianScore([90, 95, 87, 60, 59, 100, 96, 74, 69])).toEqual(87);
+  });
 });
 
-describe('changeToUpper', function(){
-  it('for each string in the array', function(){ expect(code.changeToUpper(['red', 'green', 'blue'])).toEqual(['RED', 'GREEN', 'BLUE']); });
+  describe('modeScore', function(){
+    it('should result in mode of scores in the array', function(){ expect(code.modeScore([90, 96, 87, 60, 59, 100, 96, 74, 69])).toEqual(96);
+  });
 });
-
-describe('findGreatestStrength', function(){
-  it('for the object list', function(){ expect(code.findGreatestStrength([{strength: 20, name: 'A'}, {strength: 10, name: 'B'}])).toEqual({strength: 20, name: 'A'}); });
-});
-
-
-
